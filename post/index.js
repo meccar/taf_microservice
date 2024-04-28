289e87446bfc7b9e1908bb76e73bea51f81f4df8
meccar <tung14hn@gmail.com>
2024-04-28 15:19:07 +0800

    updated account, gateway, post

---
+++ post/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const Config = require("./config");
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/", (req, res, next) => {
+    return res.status(200).json({"message":"Hello post"})
+}) 
+
+app.listen(Config.PORT, () => {
+    console.log(`Server is listening on port ${Config.PORT}`)
+})
