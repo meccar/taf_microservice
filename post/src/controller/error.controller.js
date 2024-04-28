f698549d9288f1c64eacc9fdf1b8477289ae62bf
meccar <tung14hn@gmail.com>
2024-04-28 16:21:03 +0800

    updated files

--- post/src/controller/error.controller.js
+++ post/src/controller/error.controller.js
@@ -1,37 +1,41 @@
 const sendErrorProd = (err, res) => {
-    if(err.isOperational){
-        res.status(err.statusCode).json({
-            status: err.status,
-            message: err.message,
-        })
-    } else {
-        console.error("ERROR 💥", err);
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // eslint-disable-next-line no-console
+    console.error("ERROR 💥", err);
 
-        res.status(500).json({
-            status: "error",
-            message: "Someting went wrong",
-        })
-    }
+    res.status(500).json({
+      status: "error",
+      message: "Someting went wrong",
+    });
+  }
 };
 
 const sendErrorDev = (err, res) => {
-    res.status(err.statusCode).json({
-        status: err.status,
-        error: err,
-        message: res.message,
-        stack: res.stack,
-    });
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: res.message,
+    stack: res.stack,
+  });
 };
 
-module.exports = (err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || "error";
+module.exports = (err, res) => {
+  // eslint-disable-next-line no-param-reassign
+  err.statusCode = err.statusCode || 500;
 
-    if (process.env.NODE_ENV = "developemt") {
-        sendErrorDev(err, res)
-    } else if (process.env.NODE_ENV = "production") {
-        let error = { ...err };
+  // eslint-disable-next-line no-param-reassign
+  err.status = err.status || "error";
 
-        sendErrorProd(error, res);
-    }
+  if (process.env.NODE_ENV === "developemt") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    const error = { ...err };
+
+    sendErrorProd(error, res);
+  }
 };
--- post/src/index.js
+++ post/src/index.js
@@ -6,10 +6,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/", (req, res, next) => {
-    return res.status(200).json({"message":"Hello post"})
-}) 
-
 app.listen(Config.PORT, () => {
-    console.log(`Server is listening on port ${Config.PORT}`)
-})
+  // eslint-disable-next-line no-console
+  console.log(`Server is listening on port ${Config.PORT}`);
+});
