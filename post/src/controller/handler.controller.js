ff93c4b3f2e4d11afb3139467eb1aca5706e0f84
meccar <tung14hn@gmail.com>
2024-05-24 09:44:14 +0000

    fixed bug post

--- post/src/controller/handler.controller.js
+++ post/src/controller/handler.controller.js
@@ -2,45 +2,48 @@ const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
 // const APIFeatures = require("../util/apiFeatures");
 
-exports.deleteOne = (Model) => catchAsync( async (req, res, next) => {
+exports.deleteOne = (Model) =>
+  catchAsync(async (req, res, next) => {
     const doc = await Model.findByIdAndDelete(req.params.id);
 
     if (!doc) {
-        return next(new AppError("No document found with that ID", 404))
+      return next(new AppError("No document found with that ID", 404));
     }
 
     return res.status(204).json({
-        status: "success",
-        data: null,
+      status: "success",
+      data: null,
     });
-});
+  });
 
-exports.updateOne = (Model) => catchAsync ( async (req, res, next) => {
-    const doc = await Model.findByIdAndUpdate(req.params.id)
+exports.updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id);
 
     if (!doc) {
-        return next (new AppError("No document found with that ID", 404))
+      return next(new AppError("No document found with that ID", 404));
     }
 
     return res.status(200).json({
-        status: "success",
-        data: {
-            data: doc,
-        },
+      status: "success",
+      data: {
+        data: doc,
+      },
     });
-});
+  });
 
-exports.createOne = (Model) => catchAsync ( async (req, res, next) => {
-    const doc = await Model.create(req.body)
+exports.createOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.create(req.body);
 
     if (!doc) {
-        return next (new AppError("No document found with that ID", 404))
+      return next(new AppError("No document found with that ID", 404));
     }
 
     return res.status(201).json({
-        status: "success",
-        data: {
-            data: doc
-        }
-    })
-})
+      status: "success",
+      data: {
+        data: doc,
+      },
+    });
+  });
