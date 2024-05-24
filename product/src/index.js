b0bcfbc9296d0a34d94a5eb92a4c5db18442341f
meccar <tung14hn@gmail.com>
2024-05-24 10:19:52 +0000

    updated files

---
+++ product/src/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cluster = require("cluster");
+const os = require("os");
+
+const Config = require("./config/config");
+const productRoute = require("./routes/product.route");
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/v1/product", productRoute);
+
+if (cluster.isMaster) {
+  // console.log("Master has been started ...");
+  const NUM_WORKERS = os.cpus().length;
+  for (let i = 0; i < NUM_WORKERS; i += 1) {
+    cluster.fork();
+  }
+} else {
+  // console.log("Worker process started ...");
+  app.listen(Config.PORT, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Server is listening on port ${Config.PORT}`);
+  });
+}
