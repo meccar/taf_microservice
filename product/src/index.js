882b2ceeec25a3c1412457200add30d7bbf38b3c
meccar <tung14hn@gmail.com>
2024-05-26 05:38:38 +0000

    update clusters

--- product/src/index.js
+++ product/src/index.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const cluster = require("cluster");
-const os = require("os");
 
 const Config = require("./config/config");
 const productRoute = require("./routes/product.route");
@@ -11,16 +9,8 @@ app.use(express.json());
 
 app.use("/api/v1/product", productRoute);
 
-if (cluster.isMaster) {
-  // console.log("Master has been started ...");
-  const NUM_WORKERS = os.cpus().length;
-  for (let i = 0; i < NUM_WORKERS; i += 1) {
-    cluster.fork();
-  }
-} else {
-  // console.log("Worker process started ...");
-  app.listen(Config.PORT, () => {
-    // eslint-disable-next-line no-console
-    console.log(`Server is listening on port ${Config.PORT}`);
-  });
-}
+app.listen(Config.PORT, () => {
+// eslint-disable-next-line no-console
+  console.log(`Server is listening on port ${Config.PORT}`);
+});
+
