b7b68ec6b6c4f57a3d803981d6348a39546b53e9
meccar <tung14hn@gmail.com>
2024-07-19 21:17:13 +0800

    Updates

--- common/src/middlewares/checkCache.middleware.js
+++ common/src/middlewares/checkCache.middleware.js
@@ -1,15 +1,14 @@
-const catchAsync = require("../utils/catchAsync")
-const redisManager = require("../redis/client")
+const { catchAsync } = require("../utils/catchAsync");
+const { redisManager } = require("../redis/client");
 
-const checkCache = catchAsync( async(req, res, next) => {
-    const { key } = req.params;
-    const cachedData = await redisManager.get(key);;
+const checkCache = catchAsync(async (req, res, next) => {
+  const { key } = req.params;
+  const cachedData = await redisManager.get(key);
 
-    if (cachedData) {
-        return res.json(JSON.parse(cachedData));
-    }
-    next();
-})
+  if (cachedData) {
+    return res.json(JSON.parse(cachedData));
+  }
+  next();
+});
 
-module.exports = checkCache 
-  
+module.exports = checkCache;
