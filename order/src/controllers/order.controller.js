f0fbe0d7d3dffab39910d6f785b7207126875d72
meccar <tung14hn@gmail.com>
2024-07-24 17:15:40 +0800

    updated order

--- order/src/controllers/order.controller.js
+++ order/src/controllers/order.controller.js
@@ -3,6 +3,6 @@ const { Order } = require("../models/order.model");
 
 exports.CreateOrder = handler.createOne(Order);
 exports.GetAllOrders = handler.getAll(Order);
-exports.GetOrder = handler.getOne(Order);
+exports.GetOrder = handler.getOne(Order, "product");
 exports.UpdateOrder = handler.updateOne(Order);
 exports.DeleteOrder = handler.deleteOne(Order);
--- order/src/routes/order.route.js
+++ order/src/routes/order.route.js
@@ -4,16 +4,17 @@ const router = express.Router({ mergeParams: true });
 
 const { VerifyToken } = require("@tafvn/common");
 const OrderController = require("../controllers/order.controller");
-const {redisCaching} = require("../utils/redis")
+const { redisCaching } = require("../utils/redis");
+const checkOrder = require("../middlewares/order.middleware");
 
 router
   .route("/")
   .get(redisCaching, OrderController.GetAllOrders)
-  .post(VerifyToken, redisCaching, OrderController.CreateOrder);
+  .post(VerifyToken, redisCaching, checkOrder, OrderController.CreateOrder);
 router
   .route("/:id")
-  .get(redisCaching, OrderController.GetOrder)
+  .get(VerifyToken, redisCaching, OrderController.GetOrder)
   .patch(VerifyToken, redisCaching, OrderController.UpdateOrder)
-  .delete(VerifyToken, redisCaching,OrderController.DeleteOrder);
+  .delete(VerifyToken, redisCaching, OrderController.DeleteOrder);
 
 module.exports = router;
